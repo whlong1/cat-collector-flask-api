e5a269029ea4bb3fc940ad057ca2c58b93f6e285
whlong1 <whunterlong@gmail.com>
2022-04-07 22:52:09 -0400

    deploy

--- config.py
+++ config.py
@@ -3,12 +3,7 @@ import os
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 class Config:
-  CORS_HEADERS = 'Content-Type'
+  DEBUG = True
+  SQLALCHEMY_ECHO = True
+  SQLALCHEMY_DATABASE_URI = DATABASE_URL
   SQLALCHEMY_TRACK_MODIFICATIONS = False
-  if DATABASE_URL:
-    SQLALCHEMY_ECHO = False
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL.replace("://", "ql://", 1)
-  else:
-    DEBUG=True
-    SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/cat_collector_api"
